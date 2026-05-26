6519ffb1bf8bf17f3cd6de7332b6d4a7cc68da1d
lValtiel <dev.emersonc@gmail.com>
2026-05-25 21:10:20 -0400

    Ejercicio Veintidos completado

---
+++ Veintidos.java
@@ -0,0 +1,23 @@
+//Contar palabras
+//Contar cuántas palabras tiene una frase.
+
+public class Veintidos {
+	public static void main(String[] args) {
+		String texto = "hola mundo  java";
+		int contador = 0;
+
+		for(int i=0; i<texto.length(); i++) {
+			
+			char letraActual = texto.charAt(i);
+
+			if(letraActual != ' ') {
+				
+				if(i == texto.length() - 1 || texto.charAt(i + 1) == ' ') {
+					contador++;
+				}
+			}
+		}
+
+		System.out.println(contador);
+	}
+}
